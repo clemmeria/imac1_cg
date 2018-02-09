f416cdf56fdc3a8e3bae15f399acdcd4cd6c1600
Clemmeria <cmeriade@localhost.localdomain>
2018-02-09 13:44:00 +0100

    draw one point

--- minimal.c
+++ minimal.c
@@ -21,6 +21,32 @@ static const unsigned int BIT_PER_PIXEL = 32;
 /* Nombre minimal de millisecondes separant le rendu de deux images */
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
 
+void resizeWindow(SDL_Event e){
+
+    SDL_SetVideoMode(e.resize.w, e.resize.h, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
+    glViewport(0,0,e.resize.w, e.resize.h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1.,1.,-1.,1.);
+
+}
+
+int drawObject(SDL_Event e){
+
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glPointSize(10.0f);
+
+	printf("Dessiner !\n");
+	glBegin(GL_POINTS);
+	glColor3ub(0,0,0);
+	glVertex2f(-1 + 2. * e.button.x / WINDOW_WIDTH, -(-1 +2. * e.button.y / WINDOW_HEIGHT));
+	//glEnd();
+
+	return 0;
+
+}
+
 int main(int argc, char** argv) {
 
 
@@ -30,6 +56,9 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Impossible d'initialiser la SDL. Fin du programme.\n");
         return EXIT_FAILURE;
     }
+
+    /* Desactiver le double buffering avant d'ouvrir la fenetre pour que le dessin persiste */
+    //SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,0);
     
     /* Ouverture d'une fenêtre et création d'un contexte OpenGL */
     if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE)) {
@@ -42,6 +71,7 @@ int main(int argc, char** argv) {
     
     /* Boucle d'affichage */
     glClearColor(1,1,0,1); // definir la couleur de nettoyage
+
     int loop = 1;
     while(loop) {
 
@@ -49,7 +79,7 @@ int main(int argc, char** argv) {
         Uint32 startTime = SDL_GetTicks();
         
         /* Placer ici le code de dessin */
-        glClear(GL_COLOR_BUFFER_BIT); // Nettoie la fenetre et appelle la couleur de nettoyage
+    	glClear(GL_COLOR_BUFFER_BIT); // Nettoie la fenetre et appelle la couleur de nettoyage
 
         /* Echange du front et du back buffer : mise à jour de la fenêtre */
         SDL_GL_SwapBuffers();
@@ -68,16 +98,13 @@ int main(int argc, char** argv) {
             switch(e.type) { // e.type = type d'evnt recu
 
                 case SDL_VIDEORESIZE:
+                    resizeWindow(e);
                     printf("Il y a redimmensionnement : %d , %d\n", e.resize.w, e.resize.h);
-                    SDL_SetVideoMode(e.resize.w, e.resize.h, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE);
-                    glViewport(0,0,e.resize.w, e.resize.h);
-                    glMatrixMode(GL_PROJECTION);
-                    glLoadIdentity();
-                    gluOrtho2D(-1.,1.,-1.,1.);
                     break;
 
                 /* Clic souris */
                 case SDL_MOUSEBUTTONUP:
+                	drawObject(e);
                     printf("clic en (%d, %d)\n", e.button.x, e.button.y);
                     glClearColor(e.button.x/(float)WINDOW_WIDTH,e.button.y/(float)WINDOW_HEIGHT,0,1);
                     break;
